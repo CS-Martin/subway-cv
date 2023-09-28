abafbfe9980b01b98e1e7e29cc0b5fb92dfca33e
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-09-29 00:17:30 +0800

    refactor: logs

--- src/utilities/config.py
+++ src/utilities/config.py
@@ -1,11 +1,17 @@
 import logging
+import os 
 
 def configure_logging():
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    logging_path = os.path.join('logs', 'game.log')
+
     logging.basicConfig(
         level=logging.DEBUG,
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[
-            logging.FileHandler('debug.log'),
+            logging.FileHandler(logging_path),
             logging.StreamHandler()
         ]
     )
